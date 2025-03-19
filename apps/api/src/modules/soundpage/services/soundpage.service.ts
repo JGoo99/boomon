f4c4a18563a184d4e50b635b34ef7781bf505063
JinGoo <zzgg3040@naver.com>
2025-03-19 15:49:03 +0900

    feat: soundpage create service

--- apps/api/src/modules/soundpage/services/soundpage.service.ts
+++ apps/api/src/modules/soundpage/services/soundpage.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Soundpage } from '../entities/soundpage.interface';
+import { SoundpageRepository } from '../repository/soundpage.repository';
 
 @Injectable()
-export class SoundpageService {}
+export class SoundpageService {
+  constructor(private readonly soundpageRepository: SoundpageRepository) {}
+
+  async create(soundpage: Soundpage) {
+    console.log(soundpage);
+    await this.soundpageRepository.save(soundpage);
+  }
+}
