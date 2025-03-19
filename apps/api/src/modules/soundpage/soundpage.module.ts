e2440b3507031b8b5dba1497af054291a6335fbd
JinGoo <zzgg3040@naver.com>
2025-03-19 15:47:59 +0900

    feat: soundpage entity module import

--- apps/api/src/modules/soundpage/soundpage.module.ts
+++ apps/api/src/modules/soundpage/soundpage.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { SoundpageController } from './controllers/soundpage.controller';
 import { SoundpageService } from './services/soundpage.service';
+import { DynamooseModule } from 'nestjs-dynamoose';
+import { SoundpageSchema } from './entities/soundpage.schema';
+import { CommonModule } from '../../common/common.module';
+import { SoundpageRepository } from './repository/soundpage.repository';
 
 @Module({
+  imports: [
+    CommonModule,
+    DynamooseModule.forFeature([
+      { name: 'Soundpage', schema: SoundpageSchema },
+    ]),
+  ],
   controllers: [SoundpageController],
-  providers: [SoundpageService]
+  providers: [SoundpageService, SoundpageRepository],
 })
 export class SoundpageModule {}
