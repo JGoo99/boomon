4d6bea2021d8b73a82dfa069f2d86d584fdadd17
JinGoo <zzgg3040@naver.com>
2025-03-19 15:46:48 +0900

    feat: dynamodb config

--- apps/api/src/common/common.module.ts
+++ apps/api/src/common/common.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { HealthController } from './health/health.controller';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
+import { DynamooseModule } from 'nestjs-dynamoose';
+import { DynamooseConfigService } from './model/dynamoose-config.service';
 
 @Module({
   imports: [
     TerminusModule,
-    HttpModule
+    HttpModule,
+    DynamooseModule.forRootAsync({ useClass: DynamooseConfigService }),
   ],
-  controllers: [HealthController]
+  controllers: [HealthController],
 })
 export class CommonModule {}
