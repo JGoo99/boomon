bafd94180dcd3d954390ce99c93774a678fe97d1
JinGoo <zzgg3040@naver.com>
2025-03-19 15:48:17 +0900

    feat: soundpage create controller

--- apps/api/src/modules/soundpage/controllers/soundpage.controller.ts
+++ apps/api/src/modules/soundpage/controllers/soundpage.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SoundpageService } from '../services/soundpage.service';
+import { Soundpage } from '../entities/soundpage.interface';
 
 @Controller('soundpage')
-export class SoundpageController {}
+export class SoundpageController {
+  constructor(private readonly soundpageService: SoundpageService) {}
+
+  @Post()
+  async create(@Body() soundpage: Soundpage) {
+    await this.soundpageService.create(soundpage);
+  }
+}
