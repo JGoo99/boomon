a90933f7e2006f2739539a8e7fdafa4fcc64d85e
JinGoo <zzgg3040@naver.com>
2025-03-20 11:30:10 +0900

    refactor: structure-health-directory

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './controllers/app.controller';
-import { AppService } from './app.service';
 import { SoundpageModule } from './modules/soundpage/soundpage.module';
 import { CommonModule } from './common/common.module';
 import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SoundpageModule, CommonModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    SoundpageModule,
+    CommonModule,
+    HealthModule,
+  ],
 })
 export class AppModule {}
