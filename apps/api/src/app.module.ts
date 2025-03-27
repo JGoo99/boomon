7a38f7b6e79443824f66881e7aa397920c4daf7d
yuna <yjeong1217@gmail.com>
2025-03-27 21:28:11 +0900

    feat: local 실행 분리

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SoundpageModule } from './modules/soundpage/soundpage.module';
 import { CommonModule } from './common/common.module';
 import { ConfigModule } from '@nestjs/config';
-import { HealthModule } from './modules/health/health.module';
+import { HealthModule } from './common/health/health.module';
 import { BookmarkModule } from './modules/bookmark/bookmark.module';
 
 @Module({
