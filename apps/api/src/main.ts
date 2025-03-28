17900152ac508cabbfb2780225a664c25af52064
yuna <yjeong1217@gmail.com>
2025-03-28 23:25:48 +0900

    fix: main.ts app 생성

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 import { SuccessResponseInterceptor } from './common/interceptors/success-response-interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 let isInitialized = false;
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new SuccessResponseInterceptor());
 
